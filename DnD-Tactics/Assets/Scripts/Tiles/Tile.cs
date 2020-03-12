7804d3427cc94845ac072b10f28b58a78350e8a3
Fotis Magnisalis <fotis23able@gmail.com>
2020-03-12 13:35:50 +0200

    First Commit-Tile implementetion
    Created Tile class objects and Menu tool for tile X,Y values assigment

---
+++ DnD-Tactics/Assets/Scripts/Tiles/Tile.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Vector2Int TileLocation;
+
+    public TileType tileType;
+}
---
+++ DnD-Tactics/Assets/Scripts/Tiles/TileAssigner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TileAssigner : MonoBehaviour
+{
+    [MenuItem("Tools/Assign Tile Location")]
+    public static void AssignTileLocation()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+
+
+        //Assign the position
+        foreach(GameObject t in tiles)
+        {
+
+            t.GetComponent<Tile>().TileLocation.x = (int) t.transform.position.x;
+            t.GetComponent<Tile>().TileLocation.y = (int) t.transform.position.z; //We don't care about the y in this context
+        }
+    }
+}
